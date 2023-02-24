2bead368337d78d09a59272b4b76940234333a3b
Theophilus <isahtheophilus@gmail.com>
2023-02-24 22:20:46 +0100

    chore: add eslintrc.json

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,16 @@
+/* eslint-disable no-undef */
 module.exports = {
-    // Custom:
-    semi: false,
-    trailingComma: "all",
-    printWidth: 90,
-    quoteProps: "consistent",
-    // Defaults:
-    singleQuote: false,
-    tabWidth: 2,
-    useTabs: false,
-    bracketSpacing: true,
-    arrowParens: "always",
-    proseWrap: "preserve",
-    endOfLine: "lf",
-  }
-  
+  // Custom:
+  semi: false,
+  trailingComma: "all",
+  printWidth: 90,
+  quoteProps: "consistent",
+  // Defaults:
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-undef */
 module.exports = {
-    mode: "development",
-    entry: "./src/index.tsx",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        filename: "bundle.js",
-    }
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+  },
 }
