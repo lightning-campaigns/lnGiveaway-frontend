f85db5de99b8a6c80609438611690dd040eca94d
Theophilus <isahtheophilus@gmail.com>
2023-02-25 12:05:52 +0100

    chore: add copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const path = require("path")
+const copyWebpackPlugin = require("copy-webpack-plugin")
+
 module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
@@ -11,6 +15,20 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/Assets/manifest.json"),
+          to: path.resolve(__dirname, "./dist"),
+        },
+        {
+          from: path.resolve(__dirname, "./src/Assets/icons"),
+          to: path.resolve(__dirname, "./dist"),
+        },
+      ],
+    }),
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
